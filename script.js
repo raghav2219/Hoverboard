b5adff5a2c69a93e36da8e184f529709179726d3
Raghav <abhishekraghav10096@gmail.com>
2022-08-07 15:43:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container')
+
+function randColor() {
+    let red = Math.floor(Math.random() * 255) + 1;
+    let green = Math.floor(Math.random() * 255) + 1;
+    let blue = Math.floor(Math.random() * 255) + 1;
+    return `rgb(${red}, ${green}, ${blue})`
+}
+
+const SQUARES = 500;
+
+for (let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+    const color = randColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color} , 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.background = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
